5453cb5ab5dcc14d970bff1817177bab80114647
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-17 21:30:16 +0100

    Add files via upload

---
+++ GeneralLoop/Loop.js
@@ -0,0 +1,19 @@
+// for (let i = 0; i < 10; i++){
+//     if(i === 2) {
+//         console.log("Sharp babe");
+//         continue;
+//     }
+//     if (i === 5){
+//         console.log('Stop counting ')
+//         break;
+//     }
+//     console.log(`Number ${+i}`);
+//
+// }
+const names = ['shulammite', 'Uloma', 'Onyeka', 'Bella']
+for (let i = 0; i < names.length; i++) {
+
+    console.log(names[i]);
+}
+
+// names.
