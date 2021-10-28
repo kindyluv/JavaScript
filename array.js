3830ec9e748e127ca1dd5cb473a8a0b9e2a626c2
kindyluv <onyeukwuamara@gmail.com>
2021-10-28 16:13:23 +0100

    added new file

---
+++ array.js
@@ -0,0 +1,36 @@
+let cohort8Array = ["ozioma", "jerry", "tife", "cubana", "femi"]
+for (let i = 0; i < cohort8Array.length; i++) {
+    if(cohort8Array[i] !== "tife"){
+        console.log(cohort8Array[i])
+    }
+}
+
+// for (let key in cohort8Array) {
+//     console.log(cohort8Array[key])
+// }
+
+// for (let value of cohort8Array) {
+//     console.log(value)
+// }
+// console.log(cohort8Array.splice(1,1))
+// console.log(cohort8Array)
+// let newArray = cohort8Array.slice(0, 4)
+//     console.log(newArray)
+
+// function nameType(name) {
+//     let count = 0;
+//     nameString = ""
+//     for (let i = 0; i < name.length; i++) {
+//         nameString += name[i] + ++count
+//     }
+//     console.log(nameString)
+// }
+// nameType("saheed")
+// nameType("dami")
+// nameType("jerry")
+// nameType("ozioma")
+
+//CALLBACK FUNCTION
+
+// setTimeOut(()=> {console.log("kim")}, 5000)
+// console.log("tife")
