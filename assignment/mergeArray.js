a2c232ef2f65b63a60e6721eeabf416ca2136678
kindyluv <onyeukwuamara@gmail.com>
2021-11-01 13:00:24 +0100

    ...

--- assignment/mergeArray.js
+++ assignment/mergeArray.js
@@ -1,6 +1,5 @@
 function merge(a,b){
     let c = [];
-
     for (let i = 0; i < a.length; i++) {
         if (!(c.includes(a[i]))) c.push(a[i])
         for (let j = 0; j < b.length; j++) {
@@ -13,5 +12,35 @@ function merge(a,b){
 let a = [2, 3, 1];
 let b = [2,9,5,3]
 console.log(merge(a,b));
+let mergeArray = merge(a,b)
 
 
+function sortArrayInDescendingOrder(c){
+    for (let i = 0; i < c.length; i++) {
+        for (let j =i+1; j < c.length; j++) {
+            if(c[i] < c[j]){
+                let temp = c[i];
+                c[i] = c[j];
+                c[j] = temp;
+            }
+        }
+    }
+    return c;
+}
+
+console.log("Array in descending order ", sortArrayInDescendingOrder(mergeArray))
+
+// function sortArrayInAscendingOrder(c){
+//     for (let i = 0; i < c.length; i++) {
+//         for (let j =i+1; j < c.length; j++) {
+//             if(c[j] < c[i]){
+//                 let temp = c[i];
+//                 c[i] = c[j];
+//                 c[j] = temp;
+//             }
+//         }
+//     }
+//     return c;
+// }
+//
+// console.log("Array in ascending order ", sortArrayInAscendingOrder(mergeArray));
