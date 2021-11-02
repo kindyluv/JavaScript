ebf36f56fa4ac36bbf8f1683bef54f146ba0940e
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-11-02 10:03:20 +0100

    Update Loop.js

--- GeneralLoop/Loop.js
+++ GeneralLoop/Loop.js
@@ -1,15 +1,15 @@
-// for (let i = 0; i < 10; i++){
-//     if(i === 2) {
-//         console.log("Sharp babe");
-//         continue;
-//     }
-//     if (i === 5){
-//         console.log('Stop counting ')
-//         break;
-//     }
-//     console.log(`Number ${+i}`);
-//
-// }
+for (let i = 0; i < 10; i++){
+    if(i === 2) {
+        console.log("Sharp babe");
+        continue;
+    }
+    if (i === 5){
+        console.log('Stop counting ')
+        break;
+    }
+    console.log(`Number ${+i}`);
+
+}
 const names = ['shulammite', 'Uloma', 'Onyeka', 'Bella']
 for (let i = 0; i < names.length; i++) {
 
