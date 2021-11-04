e05506449c924ba1450e7a75533b9dcc5a7475a4
kindyluv <onyeukwuamara@gmail.com>
2021-11-04 11:22:04 +0100

    ...

--- conditionalStatement/SwitchCase.js
+++ conditionalStatement/SwitchCase.js
@@ -1,39 +1,42 @@
+
 let day;
 switch (day) {
-    case 0:
-        day = 'Monday'; break
     case 1:
-        day = 'Tuesday'; break
+        day = 'Monday'; break
     case 2:
-        day = 'Wednesday'; break
+        day = 'Tuesday'; break
     case 3:
-        day = 'Thursday'; break
+        day = 'Wednesday'; break
     case 4:
-        day = 'Friday'; break
+        day = 'Thursday'; break
     case 5:
-        day = 'Saturday'; break
+        day = 'Friday'; break
     case 6:
+        day = 'Saturday'; break
+    case 7:
         day = 'Sunday'; break
     default:
         day= ("Enter the day you want");
 }
 
 switch (new Date().getDay()) {
-    case 0:
-        day = 'Monday'; break;
     case 1:
-        day = 'Tuesday'; break;
+        day = 'Monday'; break;
     case 2:
-        day = 'Wednesday'; break;
+        day = 'Tuesday'; break;
     case 3:
-        day = 'Thursday'; break;
+        day = 'Wednesday'; break;
     case 4:
-        day = 'Friday'; break;
+        day = 'Thursday'; break;
     case 5:
-        day = 'Saturday'; break;
+        day = 'Friday'; break;
     case 6:
+        day = 'Saturday'; break;
+    case 7:
         day = 'Sunday'; break;
     default:
          day= ("Enter the day you want");
 }
-console.log(`Today is ${day}`)
+const  days = new Date();
+days.getUTCDay();
+console.log(`Today is ${day} \n ${days}`);
