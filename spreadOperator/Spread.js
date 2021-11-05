9c4aad034040143e6ff07f3ed26250ac48f70319
kindyluv <onyeukwuamara@gmail.com>
2021-11-05 12:36:58 +0100

    ...

--- spreadOperator/Spread.js
+++ spreadOperator/Spread.js
@@ -8,3 +8,10 @@ let persons = {
 console.log("1", persons)
 persons = {...persons}
 console.log("\n2", persons)
+
+persons = {...persons, person4: "ozzy"}
+console.log("\n",persons)
+
+//overriding in js is updating and data immutability
+let users = {name: "segun", crush: "mojoyin", hobby: "praying", crush: "ozzy"}
+console.log("\n",users)
