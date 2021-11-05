60ad6c30d1bf5995313d7f28b23645ce3d3a04b1
kindyluv <onyeukwuamara@gmail.com>
2021-11-05 11:24:00 +0100

    ...

---
+++ spreadOperator/Spread.js
@@ -0,0 +1,10 @@
+let persons = {
+    person1: "jerry",
+    person2: "sa'eed",
+    person3: "demola",
+    person4: "dami",
+}
+
+console.log("1", persons)
+persons = {...persons}
+console.log("\n2", persons)
